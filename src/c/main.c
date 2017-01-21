7a96e664d7202c423ef2190a9818c51b9554552f
Chris Feltner <chris.feltner@knights.ucf.edu>
2017-01-21 12:25:59 -0500

    Added AppMessage implementation

--- src/c/main.c
+++ src/c/main.c
@@ -2,33 +2,38 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
-static void update_time() {
-  // Get a tm structure
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
 
-  // Write the current hours and minutes into a buffer
-  static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-                                          "%H:%M" : "%I:%M", tick_time);
-
-  // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, s_buffer);
 }
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
 
 static void main_window_load(Window *window){
   //Get info about the window 
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 	
-	//Create text layer with specific bounds for displaying time
+  //test
+	//Create text layer with specific bounds
 	s_time_layer = text_layer_create(
 	GRect(0,PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 	
 	//Improve the layout
 	text_layer_set_background_color(s_time_layer, GColorClear);
 	text_layer_set_text_color(s_time_layer, GColorBlue);
-  //text_layer_set_text(s_time_layer, "00:00");
+	//APP NAME GOES HERE
+  text_layer_set_text(s_time_layer, "BUZZ");
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 	
@@ -41,11 +46,13 @@ static void main_window_unload(Window *window){
   text_layer_destroy(s_time_layer);
   }
 
-//Gets Time
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-update_time();
-}
 static void init(){
+  
+  // Register callbacks
+app_message_register_inbox_received(inbox_received_callback);
+app_message_register_inbox_dropped(inbox_dropped_callback);
+app_message_register_outbox_failed(outbox_failed_callback);
+app_message_register_outbox_sent(outbox_sent_callback);
 
 
  //Create a window element
@@ -59,17 +66,13 @@ static void init(){
   
   //Show the window on the watch
   window_stack_push(s_main_window, true);
-	// Make sure the time is displayed from the start
-update_time();
-
-	// Register with TickTimerService
-tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit(){
   //Destroy Window
   window_destroy(s_main_window);
 }
+
 int main(void){
   init();
     app_event_loop();
